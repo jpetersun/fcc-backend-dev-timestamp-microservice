d8a2627bdfbd898565aa227669a0a470f72a2ca0
James Peterson <jpeterson903@yahoo.com>
2018-03-28 16:31:54 -0700

    feat: use moment, remove body-parser

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const moment = require('moment')
 
 const app = express()
 const port = 8000
 
-app.get('/', (req, res) => {
-  console.log(req.body)
-  res.send('yo')
+app.get('*', (req, res) => {
+
+  const time = (req.path).slice(1)
+
+  const natural = moment.unix(time)
+
+  const timeStamp = {
+    natural
+  }
+  res.json(timeStamp)
 })
 
 app.listen(port, () => console.log(`Listening on port: ${port}`))
