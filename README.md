312660602a833abc9644224dfd70a42f7e8860a8
James Peterson <jpeterson903@yahoo.com>
2018-03-29 13:00:25 -0700

    chore: init setup

--- README.md
+++ README.md
@@ -1,15 +1,1 @@
-# fcc-backend-dev-timestamp-microservice
-
-## Input:
-```
-https://immense-shore-15010.herokuapp.com/March%2029,%202018
-https://immense-shore-15010.herokuapp.com/1522306800
-```
-
-## Output:
-```
-{
-  unix: 1522306800,
-  natural: "March 29, 2018"
-}
-```
+# fcc-backend-dev-req-header-parser-microservice
--- index.js
+++ index.js
@@ -1,34 +1,10 @@
 const express = require('express')
-const moment = require('moment')
 
 const app = express()
 const PORT = process.env.PORT || 8000
 
 app.get('*', (req, res) => {
-  const time = decodeURI((req.path).slice(1))
-  const unixStr = /\b\d{10}\b/.test(time)
-  const naturalStr = /\b\w+\s\d{2},\s\d{4}\b/.test(time)
-
-  let unix = null
-  let natural = null
-
-  if (unixStr) {
-    unix = Number(time)
-    natural = moment.unix(unix).format('LL')
-  }
-
-  if (naturalStr) {
-    const date = new Date(time)
-    unix = Number(moment(date).format('X'))
-    natural = time
-  }
-
-  const timeStamp = {
-    unix,
-    natural
-  }
-
-  res.json(timeStamp)
+  res.json()
 })
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
