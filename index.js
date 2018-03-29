6d3b97e1ebd2a0a1e2f35b8592922fabf263d4f5
James Peterson <jpeterson903@yahoo.com>
2018-03-29 13:22:48 -0700

    feat: parse header for ipaddress, language, and operating system

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const address = require('address')
+const useragent = require('useragent')
 
 const app = express()
 const PORT = process.env.PORT || 8000
 
-app.get('*', (req, res) => {
-  res.json()
+app.get('/api/whoami', (req, res) => {
+  const agent = useragent.parse(req.headers['user-agent'])
+
+  const software = agent.os.toString()
+  const ipaddress = address.ip()
+  const language = req.headers['accept-language'].slice(0,5)
+
+  const headerParser = {
+    ipaddress,
+    language,
+    software
+  }
+
+  res.json(headerParser)
 })
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
