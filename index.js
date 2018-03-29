317da224f1cdf64a063abe2515e39ac172b809dc
James Peterson <jpeterson903@yahoo.com>
2018-03-29 14:03:53 -0700

    fix: use requestIp to set client's ip address

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
 const express = require('express')
-const publicIp = require('public-ip')
 const useragent = require('useragent')
+const requestIp = require('request-ip')
 
 const app = express()
 const PORT = process.env.PORT || 8000
 
+app.use(requestIp.mw())
+
 app.get('/api/whoami', (req, res) => {
   const agent = useragent.parse(req.headers['user-agent'])
 
-  let ipaddress = null
+  const ipaddress = req.clientIp
+  const software = agent.os.toString()
+  const language = req.headers['accept-language'].slice(0,5)
 
-  publicIp.v4().then(ip => {
-    ipaddress = ip
-  }).catch(e => {
-    ipaddress = 'ip address error'
-  }).then(() => {
-    const software = agent.os.toString()
-    const language = req.headers['accept-language'].slice(0,5)
+  const headerParser = {
+    ipaddress,
+    language,
+    software
+  }
 
-    const headerParser = {
-      ipaddress,
-      language,
-      software
-    }
-
-    res.json(headerParser)
-  })
+  res.json(headerParser)
 })
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
