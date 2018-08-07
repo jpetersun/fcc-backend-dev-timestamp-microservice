ab5ff2c098418d20639c676058f4d348d17fee6e
James Peterson <jpeterson903@yahoo.com>
2018-08-07 11:42:15 -0700

    feat: allow for optional time param

--- index.js
+++ index.js
@@ -5,27 +5,39 @@ const boom = require('boom')
 const app = express()
 const PORT = process.env.PORT || 8000
 
-app.get('/api/timestamp/:time', (req, res, next) => {
+app.get('/api/timestamp/:time?', (req, res, next) => {
+  // optional time parameter
   const { time } = req.params
-  const isUnixTime = /\d+/.test(Number(time))
-  const isUTCTime = /\b\w{4}-\w{2}-\w{2}\b/.test(time)
 
-  if (!isUnixTime && !isUTCTime) {
-    return next(boom.badRequest('invalid date'))
+  // validate time by creating Date objects
+  const isValidUnixTime = new Date(Number(time)).toUTCString()
+  const isValidTime = new Date(time).toUTCString()
+
+  // check for valid dates and allow for time parameter to be optional
+  if (isValidUnixTime === 'Invalid Date' && isValidTime === 'Invalid Date' && time !== undefined) {
+    return next(boom.badRequest('Invalid Date'))
   }
 
+  const isUnixTime = /\b\d{1,}\b/.test(Number(time))
   let unix = null
   let utc = null
 
   if (isUnixTime) {
     unix = Number(time)
-    utc = moment.unix(unix).utc().format('YYYY-MM-DD')
+    utc = moment.unix(unix).utc()
   }
 
-  if (isUTCTime) {
+  if (!isUnixTime && time) {
     const date = new Date(time)
     unix = Number(moment(date).utc().format('X'))
-    utc = time
+    utc = moment(time).utc()
+  }
+
+  // no time parameter, use current time
+  if (!time) {
+    const date = new Date().toUTCString()
+    unix = Number(moment(date).utc().format('X'))
+    utc = moment(date).utc()
   }
 
   const timeStamps = {
@@ -38,6 +50,7 @@ app.get('/api/timestamp/:time', (req, res, next) => {
 
 // error handler middleware using boom
 app.use((err, req, res, next) => {
+  console.log(err)
   return res.status(err.output.statusCode).json(err.output.payload)
 })
 
