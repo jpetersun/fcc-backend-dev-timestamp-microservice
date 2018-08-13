c6b16d628403078652fc3224d67664970e1e07eb
James Peterson <jpeterson903@yahoo.com>
2018-08-13 14:08:28 -0700

    chore: replace supertest end with then, catch

--- test/test.js
+++ test/test.js
@@ -7,23 +7,29 @@ describe('GET /api/timestamp/1460678400', () => {
   it('should respond with a timestamp', done => {
     request(app)
       .get('/api/timestamp/1460678400')
-      .end((err, res) => {
+      .then(res => {
         res.status.should.equal(200)
         res.type.should.equal('application/json')
         res.body.unix.should.equal(1460678400)
         res.body.utc.should.equal('2016-04-15')
         done()
-    })
+      })
+      .catch(err => {
+        console.error(err)
+      })
   })
 
   it('should respond with an error', done => {
     request(app)
       .get('/api/timestamp/abc')
-      .end((err, res) => {
+      .then(res => {
         res.status.should.equal(400)
         res.type.should.equal('application/json')
         res.body.message.should.equal('Invalid Date')
         done()
+      })
+      .catch(err => {
+        console.error(err)
       })
   })
 })
@@ -32,23 +38,29 @@ describe('GET /api/timestamp/2016-04-15T00:00:00.000Z', () => {
   it('should respond with a timestamp', done => {
     request(app)
       .get('/api/timestamp/2016-04-15T00:00:00.000Z')
-      .end((err, res) => {
+      .then(res => {
         res.status.should.equal(200)
         res.type.should.equal('application/json')
         res.body.unix.should.equal(1460678400)
         res.body.utc.should.equal('2016-04-15')
         done()
-    })
+      })
+      .catch(err => {
+        console.error(err)
+      })
   })
 
   it('should respond with an error', done => {
     request(app)
       .get('/api/timestamp/2016-04-155ZZ')
-      .end((err, res) => {
+      .then(res => {
         res.status.should.equal(400)
         res.type.should.equal('application/json')
         res.body.message.should.equal('Invalid Date')
         done()
+      })
+      .catch(err => {
+        console.error(err)
       })
   })
 })
@@ -57,23 +69,29 @@ describe('GET /api/timestamp/2017-07-20', () => {
   it('should respond with a timestamp', done => {
     request(app)
       .get('/api/timestamp/2017-07-20')
-      .end((err, res) => {
+      .then(res => {
         res.status.should.equal(200)
         res.type.should.equal('application/json')
         res.body.unix.should.equal(1500508800)
         res.body.utc.should.equal('2017-07-20')
         done()
-    })
+      })
+      .catch(err => {
+        console.error(err)
+      })
   })
 
   it('should respond with an error', done => {
     request(app)
       .get('/api/timestamp/2017-07-20T00:00:00.00Zz')
-      .end((err, res) => {
+      .then(res => {
         res.status.should.equal(400)
         res.type.should.equal('application/json')
         res.body.message.should.equal('Invalid Date')
         done()
+      })
+      .catch(err => {
+        console.error(err)
       })
   })
 })
@@ -82,10 +100,13 @@ describe('GET /api/timestamp', () => {
   it('should respond with a timestamp', done => {
     request(app)
       .get('/api/timestamp')
-      .end((err, res) => {
+      .then(res => {
         res.status.should.equal(200)
         res.type.should.equal('application/json')
         done()
+      })
+      .catch(err => {
+        console.error(err)
       })
   })
 })
@@ -94,11 +115,14 @@ describe('GET /api/timestamp/test-error', () => {
   it('should respond with an error', done => {
     request(app)
     .get('/api/timestamp/test-error')
-    .end((err, res) => {
+    .then(res => {
       res.status.should.equal(400)
       res.type.should.equal('application/json')
       res.body.message.should.equal('Invalid Date')
       done()
+    })
+    .catch(err => {
+      console.error(err)
     })
   })
 })
